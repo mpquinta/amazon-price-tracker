3bb31de10717c0f3d33d42d1ce36345106688f44
mpquinta <meganquintana30@gmail.com>
2023-11-05 20:04:30 -0800

    added email functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 import lxml
 from bs4 import BeautifulSoup
+import smtplib
 
 # define headers so Amazon doesn't think you're a robot
 HEADERS = {
@@ -9,13 +10,34 @@ HEADERS = {
     "Accept-Language": "en-US,en;q=0.9"
 }
 
+# user questionnaire
+email = input("What's your email? ")
+url = input("Enter URL for Amazon product price you want to track: ")
+desired_price = float(input("What's the lowest price you are willing to pay for the product? $"))
+
 # request to see data for amazon product
-amazon_webpage_data = requests.get("https://www.amazon.com/Headphones-Cancelling-Transparency-Bluetooth-Headphones/dp/B08PZD76NP/ref=sr_1_1_sspa?crid=2TEGG0RLMIYZG&keywords=apple+headphones&qid=1699128729&s=electronics&sprefix=apple+%2Celectronics%2C136&sr=1-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1", headers=HEADERS).text
+amazon_webpage_data = requests.get(url, headers=HEADERS).text
 # print(amazon_webpage_data)
 
-# extract price of product
 soup = BeautifulSoup(amazon_webpage_data, "lxml")
+
+# extract price of product
 dollar_amt = soup.find(name="span", class_="a-price-whole").getText()
 cent_amt = soup.find(name="span", class_="a-price-fraction").getText()
 price = float(dollar_amt + cent_amt)
-print(price)
+
+# extract product title
+product_title = soup.find(name="span", id="productTitle", class_="a-size-large product-title-word-break").getText().strip()
+product_title = product_title.encode("utf-8")
+
+# check if price of item is equal to or lower than desired price  
+if price <= desired_price:
+    # if price is lower than desired price, send user an email alert
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=os.environ["EMAIL"], password=os.environ["PW"])
+        connection.sendmail(
+            from_addr=os.environ["EMAIL"],
+            to_addrs=email,
+            msg=f"Subject:Price Drop for Amazon Wishlist Item\n\nConsider buying {product_title} now! It's now ${price}. {url}"
+        )
